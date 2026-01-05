840589c50ce4d298e6c18208f195e6a98aa10eab
P4vanzinho <thiago772000@gmail.com>
2026-01-05 11:03:48 -0300

    refactor: remove duplicate import of ReactNode in layout.tsx for cleaner code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from "react";
 import { Inter, Kumbh_Sans } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/common/Header";
-import { ReactNode } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
