279fb1e03f4960f31b05a6c3f75e5fcbd1dc0bc4
P4vanzinho <thiago772000@gmail.com>
2026-01-12 12:28:17 -0300

    feat: add Post and PostSkeleton components along with PostsList for displaying blog posts and loading states

---
+++ src/components/blog/Post.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import { Post as PostType } from "@/types/post";
+import { DirectIcon } from "@/components/common/icons/DirectIcon";
+import { LikeIcon } from "@/components/common/icons/LikeIcon";
+import { SocialStatButton } from "./SocialStatButton";
+import { ReadMoreButton } from "./ReadMoreButton";
+import { formatNumber } from "@/utils/formatter";
+
+interface PostProps {
+  post: PostType;
+}
+
+export function Post({ post }: PostProps) {
+  return (
+    <div className="flex flex-col py-10 lg:h-full lg:py-[3.75rem] 2xl:py-[5rem]">
+      <Image
+        src={post.image}
+        alt={post.imageAlt}
+        width={1000}
+        height={1000}
+        className="h-[13.3125rem] w-full rounded-lg object-cover lg:h-[13.3125rem] lg:flex-shrink-0"
+      />
+      <div className="flex flex-col gap-2 pt-4 pb-1">
+        <h2 className="font-sans text-[1.25rem] leading-[150%] font-semibold text-white">
+          {post.title}
+        </h2>
+      </div>
+
+      <div className="flex gap-5 pb-[1.125rem]">
+        <div className="flex flex-col">
+          <span className="text-grey-60 font-sans text-[0.875rem] leading-[150%]">
+            {post.category}
+          </span>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between gap-4 lg:mt-auto">
+        <div className="flex items-center gap-2 2xl:gap-[0.625rem]">
+          <SocialStatButton
+            icon={<LikeIcon className="h-5 w-5 2xl:h-6 2xl:w-6" />}
+            value={formatNumber(post.likes)}
+          />
+          <SocialStatButton
+            icon={<DirectIcon className="h-5 w-5 2xl:h-6 2xl:w-6" />}
+            value={post.shares.toString()}
+          />
+        </div>
+        <ReadMoreButton variant="regular" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/home/PostsList.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Separator } from "@/components/common/Separator";
+import { Post } from "@/components/blog/Post";
+import { PostSkeleton } from "@/components/blog/PostSkeleton";
+import { useRegularPosts } from "@/hooks/usePosts";
+
+export function PostsList() {
+  const { data: regularPosts, isLoading } = useRegularPosts();
+
+  if (isLoading) {
+    return (
+      <>
+        <PostSkeleton />
+        <Separator />
+        <PostSkeleton />
+        <Separator />
+      </>
+    );
+  }
+
+  return (
+    <div className="flex flex-col lg:flex-row lg:items-stretch lg:gap-8">
+      {regularPosts?.map((post, index) => (
+        <div key={post.id} className="min-w-0 flex-1">
+          <Post post={post} />
+          {index < regularPosts.length - 1 && (
+            <Separator className="lg:hidden" />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
