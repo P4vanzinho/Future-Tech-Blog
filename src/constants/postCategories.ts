6556f4d0b57a36ae5420ad5fa7b1402f92342740
P4vanzinho <thiago772000@gmail.com>
2026-02-11 10:05:05 -0300

    feat: introduce ArticleFilterCategory type and ARTICLE_FILTER_CATEGORIES constant for article filtering

---
+++ src/constants/postCategories.ts
@@ -0,0 +1,14 @@
+export type ArticleFilterCategory =
+  | "All"
+  | "Environment"
+  | "Politics"
+  | "Technology"
+  | "Health";
+
+export const ARTICLE_FILTER_CATEGORIES: ArticleFilterCategory[] = [
+  "All",
+  "Environment",
+  "Politics",
+  "Technology",
+  "Health",
+];
